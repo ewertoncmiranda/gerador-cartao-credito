a6cd983f1b1d963b2260d9273c80031259b5d477
Ewerton Cordeiro Miranda <sonardevplus@gmail.com>
2023-03-15 22:52:39 -0300

    Adicao das variaveis de acesso a aws

---
+++ src/main/java/miranda/app/geradorcartaocredito/Main.java
@@ -0,0 +1,12 @@
+package miranda.app.geradorcartaocredito;
+import io.quarkus.runtime.annotations.QuarkusMain;
+import io.quarkus.runtime.Quarkus;
+
+@QuarkusMain
+public class Main {
+
+    public static void main(String ... args) {
+        System.out.println("Running main method");
+        Quarkus.run(args);
+    }
+}
